d15ea41ae66b5cadc717960978ee381a2d8ab898
Leon-6 <workacc@outlook.com>
2025-12-08 12:19:18 +0800

    Update release-rule-set.sh

--- .github/release-rule-set.sh
+++ .github/release-rule-set.sh
@@ -8,7 +8,7 @@ cd sing-geoip/rule-set
 git init
 git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/lyc8503/sing-box-rules.git
+git remote add origin https://github-action:$GITHUB_TOKEN@github.com/Leon-6/sing-box-rules.git
 git branch -M rule-set-geoip
 git add .
 git commit -m "Update rule-set"
